d73a6d69d9731f0310cfe3c890979bee86642b61
Taylor H <taylor@htmlartist.net>
2021-05-18 18:21:43 -0400

    done with phase 4

--- app.js
+++ app.js
@@ -6,9 +6,27 @@ const app = express();
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  console.log(req)
-  res.render('layout',);
-  });
+  // console.log(req)
+  // res.render('layout', { method: '' });
+  res.send('Hello from Express!')
+});
+
+app.get('/*xyz', (req, res) => {
+  // console.log(req)
+  // res.render('layout', { method: '' });
+  res.send(`That's all I wrote.`)
+});
+
+app.get('/capital-letters/:word', (req, res) => {
+  console.log(req.params)
+
+  const upperCase = req.params.word.toUpperCase()
+  res.send(`${upperCase}`)
+});
+app.all('/about', (req, res) => {
+
+  res.render('layout', { method: req.method, path: req.path, randomNum: Math.random() * 100 });
+})
 
 const port = 8081;
 
